46c17b91309263f3f05d57497b44742c66f98374
Diogo Costa <dsgmcosta@gmail.com>
2020-12-26 08:56:02 +0000

    fix lint error

--- src/siteData.js
+++ src/siteData.js
@@ -1,5 +1,7 @@
-export default {
+const siteData = {
   textBlocks: [
     "Coastel traz-nos, através do seu indie pop refrescante, um desabafo romantizado de textos representativos de si mesmo.",
   ],
 }
+
+export default siteData
